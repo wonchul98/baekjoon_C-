714e27bc53b1f48c83abe02bb977c16a21dfc619
wonchul98 <shinwc1998@naver.com>
2023-02-21 00:39:24 +0900

    commit:!! commit

--- baekjoon_github_upload/baekjoon_github_upload/17136.cpp
+++ baekjoon_github_upload/baekjoon_github_upload/17136.cpp
@@ -1,15 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int map[10][10];
+int max_size = 0;
+
+int size_map[10][10] = { 0, };
+int map[10][10] = { 0, };
 int visited[10][10] = { 0, };
-void bigger(int a, int b);
-int cnt[5] = { 5, 5,5,5,5};
+int cnt[6] = { 0, 5,5,5,5,5 };
 int ans = 0;
+int min_ans = 987654321;
 bool flag = false;
+int bigger(int a, int b);
+void attach(int a, int b, int size);
+void detach(int a, int b, int size);
+void dfs();
+bool fill();
 
 int main() {
+	freopen("17136_input/input.txt", "r", stdin);
 	int input;
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
@@ -17,40 +29,88 @@ int main() {
 			map[i][j] = input;
 		}
 	}
-
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (map[i][j]&& !visited[i][j]) { bigger(i, j); }
-			if (flag) {
-				cout << "-1" << endl;
-				return 0;
-			}
-		}
+	if (fill()) {
+		cout << "0" << endl;
+		return 0;
 	}
-	cout << ans << endl;
+	dfs();
+	if (flag)cout << min_ans << endl;
+	else cout << "-1" << endl;
 }
 
-void bigger(int a, int b) {
-	cout << "bigger: " << a << " " << b << endl;
-	int save_i;
-	bool flag2 = false;
-	for (int i = 1; i < 5; i++) {
+int bigger(int a, int b) { //색종이의 최대 사이즈 리턴
+	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j <= i; j++) {
-			if (!map[a + j][b + i]  || !map[a+i][b+j]) { //하나라도 다르다면
-				flag2 = true;
-				save_i = i - 1;
-				cnt[save_i]--;
-				ans++;
-				if (cnt[save_i] < 0) flag = true;
-				break;
+			if (!map[a + j][b + i] ||visited[a+j][b+i] || !map[a+i][b+j] || visited[a+i][b+j]) { //하나라도 다르다면
+				return i; //i는 size임
 			}
 		}
-		if (flag2) break;
 	}
-	cout << "save_i: " << save_i << endl;
-	for (int i = 0; i <= save_i; i++) {
-		for (int j = 0; j <= save_i; j++) {
-			visited[a+i][b+j] = 1;
+}
+
+void attach(int a, int b, int size) {
+	ans++;
+	cnt[size]--;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			visited[a + i][b + j] = 1;
 		}
 	}
+}
+
+void detach(int a, int b, int size) {
+	ans--;
+	cnt[size]++;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			visited[a + i][b + j] = 0;
+		}
+	}
+}
+
+void dfs() {
+	if (min_ans < ans) return;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (map[i][j] && !visited[i][j]) {
+				int able = bigger(i,j);
+				while(1){
+					if (cnt[able] >= 1 && able > 0) {//붙일 색종이가 있다면
+						//cout << "i: " << i << " j: " << j << " able: " << able << endl;
+						attach(i, j, able); //해당 위치에 색종이 붙임
+						//cout << "cnt: ";
+						for (int i = 1; i < 6; i++) {
+							//cout << cnt[i] << " ";
+						}
+						//cout << endl;
+						if (fill()) {
+							flag = true;
+							//cout << "filled" << endl;
+							min_ans = min(min_ans, ans);
+						}
+						
+						dfs();
+						detach(i, j, able);//리턴이 됐다면 땐다
+					}
+					able--;//사이즈 줄임
+					if (able == 0) {
+						//cout << "back_tracking" << endl;
+						return; // 붙일 수 있는 색종이가 없는 경우 리턴
+					}
+				}
+			}
+		}
+	}
+}
+
+bool fill() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (map[i][j] && !visited[i][j]) {
+				//printf("%d, %d is not filled yet\n", i, j);
+				return false;
+			}
+		}
+	}
+	return true;
 }
