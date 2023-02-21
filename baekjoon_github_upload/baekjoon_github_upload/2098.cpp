39e1a1264b6f5bf7150e8dab04085881ecf464e2
wonchul98 <89337820+wonchul98@users.noreply.github.com>
2023-02-21 21:38:45 +0900

    2098 ㅠㅠ

--- baekjoon_github_upload/baekjoon_github_upload/2098.cpp
+++ baekjoon_github_upload/baekjoon_github_upload/2098.cpp
@@ -6,8 +6,8 @@ using namespace std;
 #define INF 987654321
 int N;
 int map[17][17];
-int dp[17][17];
-int dp_f(bitset<16>vi, bitset<16> A);
+int dp[17][1 << 16];
+int dp_f(bitset<17>vi, bitset<17> A);
 int min_cost = INF;
 
 int main() {
@@ -19,13 +19,14 @@ int main() {
 			cin >> map[i][j];
 		}
 	}
-	bitset <16> first((1<<N)-1);
+	memset(dp, -1, sizeof(dp));
+	bitset <17> first((1<<N)-2);
 	cout << dp_f(1, first) << endl;
 	return 0;
 }
 
-int dp_f(bitset<16>vi, bitset<16> A) {
-	//cout << "dp_f: " << vi << " " << A << endl;
+int dp_f(bitset<17>vi, bitset<17> A) {
+	cout << "dp_f: " << vi << " " << A << endl;
 	int minimum = INF;
 	int i;
 	for (int k = 0; k < N; k++) {
@@ -33,23 +34,28 @@ int dp_f(bitset<16>vi, bitset<16> A) {
 			i = k + 1;
 		}
 	}
+	if (dp[i][(int)A.to_ulong()]!=-1) return dp[i][(int)A.to_ulong()];
 	if (A == 0) {
-		//cout << "zero" << endl;
-		//cout << "return of dp_f: " << vi << " " << A << ": " << map[i][1] << endl;
-		return map[i][1];
+		cout << "zero" << endl;
+		cout << "return of dp_f: " << vi << " " << A << ": " << map[i][1] << endl;
+		if (map[i][1]!=0)return map[i][1];
+		return INF;
 	}
-	//cout << "i: " << i << endl; //vi구하기
+	cout << "i: " << i << endl; //vi구하기
 	for (int j = 0; j < N; j++) {
 		if (A.test(j)) {
-			bitset<16>vj(1 << j);
-			//cout << "A:" << A << " j(경유지) : " << j+1 << " vj : " << vj << endl;
-			bitset<16> A_vj(A & ~vj);
-			//cout << "j갔다가 갈 곳들: " << A_vj << endl;
+			bitset<17>vj(1 << j);
+			cout << "A:" << A << " j(경유지) : " << j+1 << " vj : " << vj << endl;
+			bitset<17> A_vj(A & ~vj);
+			cout << "j갔다가 갈 곳들: " << A_vj << endl;
+			if (map[i][j + 1] > minimum) return INF;
 			int rst = dp_f(vj, A_vj);
-			//cout << "so rst: " << rst <<endl;
+			dp[j+1][(int)A_vj.to_ulong()] = rst;
 			minimum = min(minimum, map[i][j+1]+ rst);
+			cout << "so rst: " << rst << " map: " << map[i][j + 1] << " minimum: " << minimum << endl;
+			cout << "rst + map: " << map[i][j + 1] + rst << endl;
 		}
 	}
-	//cout << "return of dp_f: " << vi << " " << A <<": " << minimum << endl;
+	cout << "return of dp_f: " << vi << " " << A <<": " << minimum << endl;
 	return minimum;
 }
