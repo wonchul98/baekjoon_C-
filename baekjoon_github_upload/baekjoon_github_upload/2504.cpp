1ee74eea92b2e3fb790a8109e742914c1be23a65
wonchul98 <89337820+wonchul98@users.noreply.github.com>
2023-03-12 18:26:58 +0900

    10799, 2504 commit

--- baekjoon_github_upload/baekjoon_github_upload/2504.cpp
+++ baekjoon_github_upload/baekjoon_github_upload/2504.cpp
@@ -1,24 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<stack>
 #include<cstring>
 using namespace std;
 
 int main() {
-	stack<char> s;
+	stack<char> op_s[15];
+	stack<int> num_s[15];
+	int cur_s = -1;
 	string input;
-	char one = '1';
-	cout << (int)one << endl;
-	//cin >> input;
+	//freopen("input/2504_input.txt", "r", stdin);
+	cin >> input;
+	int ans = 0;
+	bool valid = true;
+	stack<char> v;
 	for (int i = 0; i < input.size(); i++) {
-		s.push(input[i]);
 		if (input[i] == ']') {
-			if(s.top() == '['){
-				s.pop();
-				s.push(51);
+			if (v.empty() || v.top()!='[') {
+				valid = false;
+				break;
 			}
-			if((int)s.top() - 58 <= 0){}
-
+			else {
+				v.pop();
+			}
+		}
+		else if (input[i] == ')') {
+			if (v.empty() || v.top() != '(') {
+				valid = false;
+				break;
+			}
+			else {
+				v.pop();
+			}
+		}
+		else {
+			v.push(input[i]);
 		}
 	}
-	return 0;
+	if (valid) {
+		for (int i = 0; i < input.size(); i++) {
+			//printf("input: %c, cur_s: %d\n", input[i], cur_s);
+			if (input[i] == '(') {
+				if (i > 0 && (input[i - 1] == ')' || input[i - 1] == ']') && cur_s > -1) {
+					op_s[cur_s].push('+');
+					//printf("op_s[%d].push('+')\n", cur_s);
+				}
+				else if (i > 0 && (input[i - 1] == '(' || input[i - 1] == '[') && cur_s > -1) {
+					op_s[cur_s].push('*');
+					//printf("op_s[%d].push('*')\n", cur_s);
+				}
+				cur_s++;
+				num_s[cur_s].push(2);
+				//printf("cur_s: %d, push 2\n", cur_s);
+			}
+			if (input[i] == '[') {
+				if (i > 0 && (input[i - 1] == ')' || input[i - 1] == ']') && cur_s > -1) {
+					op_s[cur_s].push('+');
+					//printf("op_s[%d].push('+')\n", cur_s);
+				}
+				else if (i > 0 && (input[i - 1] == '(' || input[i - 1] == '[') && cur_s > -1) {
+					op_s[cur_s].push('*');
+					//printf("op_s[%d].push('*')\n", cur_s);
+				}
+				cur_s++;
+				num_s[cur_s].push(3);
+				//printf("cur_s: %d, push 3\n", cur_s);
+			}
+			if (input[i] == ')' || input[i] == ']') {//cur_stack에 대해 연산
+				int rst = num_s[cur_s].top();
+				num_s[cur_s].pop();
+				while (!op_s[cur_s].empty()) {
+					if (op_s[cur_s].top() == '+') {
+						rst += num_s[cur_s].top();
+						num_s[cur_s].pop();
+						op_s[cur_s].pop();
+					}
+					else {
+						rst *= num_s[cur_s].top();
+						num_s[cur_s].pop();
+						op_s[cur_s].pop();
+					}
+				}
+				cur_s--;
+				if (cur_s >= 0) {
+					num_s[cur_s].push(rst);
+					//printf("num_s[%d].push(%d)\n", cur_s, rst);
+				}
+				else {
+					ans += rst;
+					//printf("ans: %d\n", ans);
+				}
+			}
+		}
+	}
+	cout << ans << endl;
+	
 }
