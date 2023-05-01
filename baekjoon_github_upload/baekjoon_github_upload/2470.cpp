7d443e5d35fbf09be2e65a8d9762ae2627b824aa
wonchul98 <shinwc1998@naver.com>
2023-05-02 01:41:36 +0900

    2470 수정

--- baekjoon_github_upload/baekjoon_github_upload/2470.cpp
+++ baekjoon_github_upload/baekjoon_github_upload/2470.cpp
@@ -8,35 +8,23 @@ int main() {
 	int N, minimum = 2000000000;
 	int arr[100001];
 	cin >> N;
-	int plus_count = 0, minus_count = 0;
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
-		if (arr[i] > 0) plus_count++;
-		else minus_count++;
 	}
 	sort(arr, arr+N);
-	for (int i = 0; i < N; i++) {
-		//cout << arr[i] << " ";
-	}
-	//cout << endl;
-	//if (N == plus_count) cout << arr[0] << " " << arr[1]<<endl;
-	//else if (N == minus_count) cout << arr[N-2] << " " << arr[N-1] << endl;
-	//else {
-		int left = 0, right = N - 1, val = arr[left] + arr[right];
-		int left_ans = arr[left], right_ans = arr[right];
-		while (left < right) {
-			//cout << "left: " << left << " right: " << right << endl;
-			val = arr[left] + arr[right];
-			if (abs(minimum) > abs(val)) {
-				minimum = val;
-				//cout << "minimum: " << minimum << endl;
-				left_ans = arr[left];
-				right_ans = arr[right];
-			}
-			if (val == 0)break;
-			else if (val > 0) right--;
-			else left++;
+	int left = 0, right = N - 1, val = arr[left] + arr[right];
+	int left_ans = arr[left], right_ans = arr[right];
+	while (left < right) {
+		val = arr[left] + arr[right];
+		if (abs(minimum) > abs(val)) {
+			minimum = val;
+			left_ans = arr[left];
+			right_ans = arr[right];
 		}
-		cout << left_ans << " " << right_ans << endl;
-	//}
+		if (val == 0)break;
+		else if (val > 0) right--;
+		else left++;
+	}
+	cout << left_ans << " " << right_ans << endl;
+	
 }
