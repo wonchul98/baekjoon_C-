4847a9000c64a9b1826b39b90d073f48bf1022aa
wonchul98 <shinwc1998@naver.com>
2023-07-31 23:05:56 +0900

    1854 커밋

--- baekjoon_github_upload/baekjoon_github_upload/1854.cpp
+++ baekjoon_github_upload/baekjoon_github_upload/1854.cpp
@@ -9,14 +9,12 @@ using namespace std;
 void setting();
 void dijkstra();
 void solution();
-void sorting();
+void answer();
 
-bool print = true;
+bool print = false;
 int n, m, k;
-vector<vector<int>> dist(1000,vector<int>(1000,INF));
-vector<vector<int>> town_dist;
-int min_dist[1001]; // 0 -> i 까지의 최소 거리
-bool visited[1001];
+vector<vector<int>> dist(1001,vector<int>(1001,INF));
+vector<priority_queue<int, vector<int> , less<int>>> town_dist(1001); //내림차순
 
 struct cmp {
 	bool operator() (pair<int, int>& a, pair<int, int>& b) {
@@ -36,75 +34,64 @@ int main() {
 
 void setting() {
 	if (print) cout << "setting" << endl;
-	freopen("input/1854_input.txt", "r", stdin);
+	//freopen("input/1854_input.txt", "r", stdin);
 	cin >> n >> m >> k;
-
-	// 간선 정보 저장
-	/*for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << i << j << endl;
-			dist[i].push_back(INF);
-		}
-	}*/
-	cout << "1" << endl;
 	for (int i = 1; i <= m; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		dist[a][b] = c;
 	} 
-	cout << "1" << endl;
-	//min_dist 초기화
-	for (int i = 1; i <= n; i++) {
-		min_dist[i] = INF;
-	}
-	cout << "1" << endl;
-	min_dist[1] = 0;
+
+	town_dist[1].push(0); //시작점->시작점의 최소값은 0
 }
 
 void solution() {
 	setting();
 	dijkstra();
-	sorting();
+	answer();
 }
 	
 void dijkstra() {
-	if (print) cout << "dijkstra" << endl;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq; //second기준 오름차순
 	pq.push(make_pair(1, 0));
+
 	while (!pq.empty()) {
 		int current = pq.top().first; //현재 노드 번호
 		int distance = pq.top().second; //거리
-		printf("현재 노드: (%d, %d)\n", current, distance);
 		pq.pop();
-		if (min_dist[current] < distance) continue; //최소가 아닌경우 continue
+		//if (min_dist[current] < distance) continue; //최소가 아닌경우 continue
+		if (print)printf("현재 노드: (%d, %d)\n", current, distance);
 
 		for (int i = 1; i <= n; i++) {
 			int next, next_dist;
-			//인접 정보
-			if (dist[current][i] == INF) continue; 
+			//인접 노드
+			if (dist[current][i] == INF) continue; //간선이 없는 경우
 			else {
 				next = i; //인접 노드 번호
 				next_dist = distance + dist[current][next];
 			}
-			printf("next: %d, next_dist:%d\n", next, next_dist);
-			town_dist.at(next).push_back(next_dist);
-			printf("town_dist[%d]에 %d push\n", next, next_dist);
-			if (next_dist < min_dist[next]) {//current를 지나서 가는게 더 빠른 경우
-				min_dist[next] = next_dist; //min_dist 수정
+			if (print)printf("next: %d, next_dist:%d, size: %d\n", next, next_dist, town_dist.at(next).size());
+			if (town_dist.at(next).size() < k) { //k만큼 아직 못채운 경우
+				town_dist.at(next).push(next_dist);
 				pq.push(make_pair(next, next_dist));//pq에 삽입
 			}
+			else if (town_dist.at(next).top() > next_dist) { //k만큼 채웠는데 갱신
+				if (print) cout << " top 갱신 " << endl;
+				town_dist.at(next).pop();
+				town_dist.at(next).push(next_dist);
+				pq.push(make_pair(next, next_dist));//pq에 삽입
+			}
+			// k만큼 채웠지만 갱신을 못하는 경우에는 pq에 다시 넣지 않는다. 
 		}
 	}
 }
 
-void sorting() {
-	if (print) cout << "sorting" << endl;
+void answer() {
 	for (int i = 1; i <= n; i++) {
-		sort(town_dist[i].begin(), town_dist[i].end());
+		if(print)cout << "i: " << i << endl;
 		if (k > town_dist[i].size()) cout << "-1" << endl;
-		else cout << town_dist[i][k] << endl;
+		else cout << town_dist[i].top() << endl;
 	}
-
 }
 
 
